75ff0ee26ad759df2fb3b4c3e24c4fd57260f267
Johnny Behague <johnny.behague@live.fr>
2018-04-17 13:19:15 +0200

    first version

---
+++ Domains.Infrastructure/Domains.Infrastructure/NoMediator.cs
@@ -0,0 +1,24 @@
+﻿namespace LilaSoft.Architectures.Domains.Infrastructure
+{
+    using MediatR;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class NoMediator : IMediator
+    {
+        public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default(CancellationToken)) where TNotification : INotification
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult<TResponse>(default(TResponse));
+        }
+
+        public Task Send(IRequest request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
